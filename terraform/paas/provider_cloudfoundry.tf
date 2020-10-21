9e1dba13293afe75b34a11d67dedbfd182958f15
maxmills <8balldigitalsolutions@gmail.com>
2020-10-21 11:39:20 +0100

    build change

---
+++ terraform/paas/provider_cloudfoundry.tf
@@ -0,0 +1,68 @@
+provider "cloudfoundry" {
+  api_url  = "https://api.london.cloud.service.gov.uk"
+  user     = var.user
+  password = var.password
+}
+
+terraform {
+  required_version = ">= 0.13.4"
+  required_providers {
+    cloudfoundry = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "0.12.6"
+    }
+  }
+}
+
+data "cloudfoundry_org" "my_org" {
+  name = "dfe-childrens-social-care-services"
+  // var.cf_org
+}
+
+resource "cloudfoundry_space" "placement-alpha-development" {
+  name = "placement-alpha-development"
+  // var.space
+  org = data.cloudfoundry_org.my_org.id
+}
+
+data "cloudfoundry_service" "postgres" {
+  name = "postgres"
+}
+
+resource "cloudfoundry_service_instance" "postgres" {
+  name = "development-db"
+  space = cloudfoundry_space.sandbox.id
+  service_plan = "medium11"
+}
+
+resource "cloudfoundry_domain" "default_domain" {
+  sub_domain = "childrens-social-care-placement"
+  domain = "london.cloudapps.digital"
+}
+
+resource "cloudfoundry_route" "childrens-social-care-placement-dev" {
+  domain = cloudfoundry_domain.default_domain.id
+  space = cloudfoundry_space.sandbox.id
+}
+
+resource "cloudfoundry_buildpack" "ruby" {
+  name = "ruby-buildpack"
+  path = "https://github.com/cloudfoundry/ruby-buildpack/archive/master.zip"
+  position = "1"
+}
+
+resource "cloudfoundry_buildpack" "node" {
+  name = "nodejs-buildpack"
+  path = "https://github.com/cloudfoundry/nodejs-buildpack/archive/master.zip"
+  position = "2"
+}
+
+resource "cloudfoundry_app" "childrens-social-care-placement" {
+  name = "childrens-social-care-placement"
+  space = cloudfoundry_space.sandbox.id
+  path = "https://github.com/DFE-Digital/childrens-social-care-placement/archive/master.zip"
+  buildpack = ""
+  service_binding {
+    service_instance = cloudfoundry_service_instance.postgres.id
+  }
+}
