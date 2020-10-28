dd5466badaf6508eb9ea42646ea0937a5815db72
maxmills <8balldigitalsolutions@gmail.com>
2020-10-28 13:03:02 +0000

    partial config

--- terraform/paas/terraform.tf
+++ terraform/paas/terraform.tf
@@ -8,10 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "${var.bucket_name}"
     region = "eu-west-2"
-    access_key = "${var.aws_access_key_id}"
-    secret_key = "${var.aws_secret_access_key}"
     key = "store/terraform.tfstate"
   }
 
