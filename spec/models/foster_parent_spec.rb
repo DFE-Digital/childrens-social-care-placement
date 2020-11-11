e8ae7547b460889eabffffbea76dc9f553c1d300
maxmills <8balldigitalsolutions@gmail.com>
2020-11-11 09:15:06 +0000

    have_many expectations for parent and child

--- spec/models/foster_parent_spec.rb
+++ spec/models/foster_parent_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe FosterParent, type: :model do
   it { is_expected.to belong_to(:user).required.inverse_of(:foster_parent) }
   it { is_expected.to have_many(:placements).inverse_of(:foster_parent) }
+  it { is_expected.to have_many(:children).through(:placements) }
 
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
