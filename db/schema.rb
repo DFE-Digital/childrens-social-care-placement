45c9f84c59545ab8cd1a5d211273941031f66fde
Jakub Novotny <novotny.jakub@gmail.com>
2020-11-12 10:18:18 +0000

    Add migration for Matchmakers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_094934) do
+ActiveRecord::Schema.define(version: 2020_11_12_101452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2020_11_12_094934) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foster_parents_on_user_id"
+  end
+
+  create_table "matchmakers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "first_name", null: false
+    t.string "middle_name"
+    t.string "last_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_matchmakers_on_user_id"
   end
 
   create_table "placements", force: :cascade do |t|
