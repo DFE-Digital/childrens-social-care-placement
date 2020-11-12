61af5f69589136278c1c39d486f678aef1fd86f2
Jakub Novotny <novotny.jakub@gmail.com>
2020-11-12 17:14:01 +0000

    Add feature test for matchmaker sign in

--- spec/features/user_start_spec.rb
+++ spec/features/user_start_spec.rb
@@ -2,8 +2,7 @@ require "rails_helper"
 
 RSpec.feature "User can start using the system by signing in and being redirected to the right place", type: :feature do
   context "User is a foster parent" do
-    let(:user) { FactoryBot.create(:user) }
-    let!(:foster_parent) { FactoryBot.create(:foster_parent, user: user) }
+    let(:user) { create(:foster_parent).user }
 
     scenario "User is not signed in, clicks Start on the homepage, signs in and is redirected to their dashboard page" do
       visit "/"
@@ -27,8 +26,33 @@ RSpec.feature "User can start using the system by signing in and being redirecte
     end
   end
 
-  context "User is not a foster parent" do
-    let(:user) { FactoryBot.create(:user) }
+  context "User is a matchmaker" do
+    let(:user) { create(:matchmaker).user }
+
+    scenario "User is not signed in, clicks Start on the homepage, signs in and is redirected to their dashboard page" do
+      visit "/"
+
+      click_on "Start"
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Sign in"
+
+      expect(page).to have_content("Search and shortlist foster families")
+    end
+
+    scenario "User is already signed in, clicks Start on the homepage and is redirected to their dashboard page" do
+      sign_in(user)
+
+      visit "/"
+
+      click_on "Start"
+
+      expect(page).to have_content("Search and shortlist foster families")
+    end
+  end
+
+  context "User is of an 'unspecified' type" do
+    let(:user) { create(:user) }
 
     scenario "User is not signed in, clicks Start on the homepage, signs in and is redirected back to the home page" do
       visit "/"
