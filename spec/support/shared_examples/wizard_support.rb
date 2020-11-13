9d1181540cb113fdb425f9158e0a0f34711103ae
maxmills <8balldigitalsolutions@gmail.com>
2020-11-13 11:35:17 +0000

    test wizard base

--- spec/support/shared_examples/wizard_support.rb
+++ spec/support/shared_examples/wizard_support.rb
@@ -1,5 +1,5 @@
 RSpec.shared_context "wizard store" do
-  let(:backingstore) { { "event_options" => "home_life" } }
+  let(:backingstore) { { "name" => "Joe", "age" => 35 } }
   let(:wizardstore) { Wizard::Store.new backingstore }
 end
 
@@ -13,4 +13,23 @@ end
 RSpec.shared_examples "a wizard step" do
   it { expect(subject.class).to respond_to :key }
   it { is_expected.to respond_to :save! }
+end
+
+class TestWizard < Wizard::Base
+  class Name < Wizard::Step
+    attribute :name
+    validates :name, presence: true
+  end
+
+  class Age < Wizard::Step
+    attribute :age, :integer
+    validates :age, presence: true
+  end
+
+  class Postcode < Wizard::Step
+    attribute :postcode
+    validates :postcode, presence: true
+  end
+
+  self.steps = [Name, Age, Postcode].freeze
 end
