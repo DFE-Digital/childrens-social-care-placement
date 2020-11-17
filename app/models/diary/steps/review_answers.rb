982ff8449b9467689a49f44c440d3ee757a42892
maxmills <8balldigitalsolutions@gmail.com>
2020-11-17 10:20:21 +0000

    date and time helpers

--- app/models/diary/steps/review_answers.rb
+++ app/models/diary/steps/review_answers.rb
@@ -1,5 +1,9 @@
 module Diary::Steps
   class ReviewAnswers < Wizard::Step
     # do review answers stuff here
+
+    def answers_by_step
+      @answers_by_step ||= @wizard.reviewable_answers_by_step
+    end
   end
 end
