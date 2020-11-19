3a08991f679cf619aaf0f1a0ac344e19a09e760c
Jakub Novotny <novotny.jakub@gmail.com>
2020-11-19 14:08:37 +0000

    DRY steps controllers
    I think it's becoming clear that it would be cleaner to abstract some of
    the controller template methods into it's own classes. But we'll see how
    it evolves.

--- app/controllers/diary/steps_controller.rb
+++ app/controllers/diary/steps_controller.rb
@@ -8,14 +8,9 @@ module Diary
     def step_path(step = params[:id])
       placement_diary_step_path(placement_id: params[:placement_id], id: step)
     end
-    helper_method :step_path
 
-    def wizard_store
-      ::Wizard::Store.new session_store
-    end
-
-    def session_store
-      session[:diary] ||= {}
+    def wizard_store_key
+      :diary
     end
 
     def set_page_title
