33d3d719f1de97af7146515e7499ea8c2bc3e963
maxmills <8balldigitalsolutions@gmail.com>
2020-11-20 14:39:34 +0000

    private method

--- app/policies/diary_entry_policy.rb
+++ app/policies/diary_entry_policy.rb
@@ -3,15 +3,17 @@ class DiaryEntryPolicy < ApplicationPolicy
     @auth_context.role_model.is_a?(FosterParent) && valid_placement
   end
 
-  def valid_placement
-    @auth_context.role_model.placements.pluck(:id).include? @record.placement_id
-  end
-
   class Scope < Scope
     def resolve
       if @auth_context.role_model.is_a?(FosterParent)
         scope.where(placement_id: @auth_context.role_model.placements)
       end
     end
+  end
+
+private
+
+  def valid_placement
+    @auth_context.role_model.placements.pluck(:id).include? @record.placement_id
   end
 end
