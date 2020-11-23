6e6f1f036540fb8f58785ad6ca5a3cc5cc23bfcc
Jakub Novotny <novotny.jakub@gmail.com>
2020-11-23 15:08:52 +0000

    Add date of birth field to child creation

--- spec/features/short_listing/create_a_child_spec.rb
+++ spec/features/short_listing/create_a_child_spec.rb
@@ -16,6 +16,9 @@ RSpec.feature "Matchmaker creates a new Child", type: :feature do
   scenario "Matchmaker fills in the Child's details correctly, confirms creation and lands on the shortlist page" do
     fill_in "First name", with: child_attributes[:first_name]
     fill_in "Last name", with: child_attributes[:last_name]
+    fill_in "Day", with: child_attributes[:date_of_birth].day
+    fill_in "Month", with: child_attributes[:date_of_birth].month
+    fill_in "Year", with: child_attributes[:date_of_birth].year
     click_on "Continue"
 
     expect(page).to have_content("Check this is the child you want to find a foster family for?")
@@ -32,17 +35,26 @@ RSpec.feature "Matchmaker creates a new Child", type: :feature do
 
     expect(page).to have_content("There is a problem")
     expect(page).to have_content("Enter the first name")
+    expect(page).to have_content("Enter the date of birth")
   end
 
   scenario "Matchmaker fills in the Child's details correctly, decides to change name and lands on the form again" do
     fill_in "First name", with: child_attributes[:first_name]
     fill_in "Last name", with: child_attributes[:last_name]
+    fill_in "Day", with: child_attributes[:date_of_birth].day
+    fill_in "Month", with: child_attributes[:date_of_birth].month
+    fill_in "Year", with: child_attributes[:date_of_birth].year
     click_on "Continue"
 
-    click_on "Change"
+    within find(".govuk-summary-list__row", match: :first) do
+      click_on "Change"
+    end
 
     expect(page).to have_content("Enter a child's details")
     expect(page).to have_field("First name", with: child_attributes[:first_name])
     expect(page).to have_field("Last name", with: child_attributes[:last_name])
+    expect(page).to have_field("Day", with: child_attributes[:date_of_birth].day)
+    expect(page).to have_field("Month", with: child_attributes[:date_of_birth].month)
+    expect(page).to have_field("Year", with: child_attributes[:date_of_birth].year)
   end
 end
--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Child, type: :model do
   it { is_expected.to have_many(:placements).inverse_of(:child) }
   it { is_expected.to have_many(:foster_parents).through(:placements) }
 
+  it { is_expected.to validate_presence_of(:date_of_birth) }
+
   it_behaves_like "name identifiable model" do
     let(:model_class) { described_class }
   end
