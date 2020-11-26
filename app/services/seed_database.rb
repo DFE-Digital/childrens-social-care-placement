1f34ccf3d763af72ef8507ce335be83c6d45c209
maxmills <8balldigitalsolutions@gmail.com>
2020-11-26 11:51:16 +0000

    test for seed_database

---
+++ app/services/seed_database.rb
@@ -0,0 +1,50 @@
+class SeedDatabase
+  # takes data from seed_data.rb
+
+  def initialize(seed_data)
+    @seed_data = seed_data
+  end
+
+  def call
+    truncate_tables
+    ::SeedData::DUAL_SEEDS.each do |model|
+      dual_seed(model, hash_name(model))
+    end
+    ::SeedData::SINGLE_SEEDS.each do |model|
+      single_seed(model, hash_name(model))
+    end
+  end
+
+private
+
+  def truncate_tables
+    tables = %w[users foster_parents matchmakers children placements diary_entries]
+    tables.each do |table|
+      ActiveRecord::Base.connection.execute("TRUNCATE #{table}")
+    end
+  end
+
+  def hash_name(model)
+    hash_name = "@seed_data." + model.to_s.pluralize.underscore
+    instance_eval hash_name
+  end
+
+  def dual_seed(model, hash_array)
+    hash_array.each do |hash|
+      user = User.create!(
+        email: hash[:email],
+        password: "test1234",
+      )
+      model.create!(
+        hash.merge({ user_id: user.id })
+            .except(:email),
+      )
+    end
+  end
+
+  def single_seed(model, hash_array)
+    hash_array.each do |hash|
+      model.create! hash
+    end
+  end
+end
---
+++ spec/services/seed_database_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe SeedDatabase do
+  seed_data = SeedData.new
+  subject { described_class.new(seed_data) }
+
+  describe "#call" do
+    it "seeds the database from the SeedData class" do
+      subject.call
+      expect(User.count).to eq 11
+      expect(User.first.email).to eq("r.johnston@example.com")
+      expect(FosterParent.count).to eq 10
+      expect(FosterParent.first.first_name).to eq("Rodger")
+      expect(Matchmaker.count).to eq 1
+      expect(Matchmaker.first.first_name).to eq("Paul")
+      expect(Child.count).to eq 10
+      expect(Child.first.first_name).to eq("Fredrick")
+      expect(Placement.count).to eq 1
+      expect(DiaryEntry.count).to eq 10
+    end
+  end
+end
