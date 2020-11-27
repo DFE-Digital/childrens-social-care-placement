743980d60741c5ba7c711cfb14a7d248b2eb014f
maxmills <8balldigitalsolutions@gmail.com>
2020-11-27 11:54:48 +0000

    profile must have child

--- app/models/placement_need_profile.rb
+++ app/models/placement_need_profile.rb
@@ -1,3 +1,3 @@
 class PlacementNeedProfile < ApplicationRecord
-  belongs_to :child, inverse_of: :placement_need_profile, optional: true
+  belongs_to :child, inverse_of: :placement_need_profile
 end
