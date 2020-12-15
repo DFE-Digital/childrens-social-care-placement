e774b30ec962ffba5e3cdef5ae04b1a9f72fedd0
Jakub Novotny <novotny.jakub@gmail.com>
2020-12-15 13:14:07 +0000

    Use Shortlist policy scope

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,6 +8,9 @@ class DashboardsController < ApplicationController
   def matchmaker
     authorize :dashboard
 
-    @shortlists = Shortlist.joins(:placement_need).left_outer_joins(placement_need: :placement).where(placements: { id: nil })
+    @shortlists = policy_scope(Shortlist)
+      .joins(:placement_need)
+      .left_outer_joins(placement_need: :placement)
+      .where(placements: { id: nil })
   end
 end
--- spec/policies/shortlist_policy_spec.rb
+++ spec/policies/shortlist_policy_spec.rb
@@ -17,4 +17,26 @@ RSpec.describe ShortlistPolicy do
 
     it { is_expected.to forbid_actions(%i[create show]) }
   end
+
+  describe described_class::Scope do
+    let!(:shortlists) { create_list(:shortlist, 2) }
+
+    describe "#resolve" do
+      subject(:resolve) { described_class.new(auth_context, Shortlist).resolve }
+
+      context "for matchmaker" do
+        let(:user) { create(:matchmaker).user }
+
+        it "allows all shortlists to be returned" do
+          is_expected.to match_array(shortlists)
+        end
+      end
+
+      context "for other types of users" do
+        let(:user) { FactoryBot.build_stubbed(:user) }
+
+        it { is_expected.to be_empty }
+      end
+    end
+  end
 end
